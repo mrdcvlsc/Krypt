2530a8ce15e1db9e595f4a9137bd6266209814b2
mrdcvlsc <mordecaivelasco@gmail.com>
2022-01-12 14:42:38 +0800

    fixed nested namespace in key_epansion.cpp

--- src/blockcipher/AES/key_expansion.cpp
+++ src/blockcipher/AES/key_expansion.cpp
@@ -3,52 +3,55 @@
 
 #include "../../blockcipher.hpp"
 
-namespace Krypt::BlockCipher
+namespace Krypt
 {
-    void AES::KeyExpansion(const Bytes* key)
+    namespace BlockCipher
     {
-        Bytes *w = new Bytes[4 * Nb * (Nr + 1)];
-        Bytes *temp = new Bytes[4];
-        Bytes *rcon = new Bytes[4];
-
-        int i = 0;
-        while (i < 4 * Nk)
+        void AES::KeyExpansion(const Bytes* key)
         {
-            w[i] = key[i];
-            i++;
-        }
+            Bytes *w = new Bytes[4 * Nb * (Nr + 1)];
+            Bytes *temp = new Bytes[4];
+            Bytes *rcon = new Bytes[4];
 
-        i = 4 * Nk;
-        while (i < 4 * Nb * (Nr + 1))
-        {
-            temp[0] = w[i - 4 + 0];
-            temp[1] = w[i - 4 + 1];
-            temp[2] = w[i - 4 + 2];
-            temp[3] = w[i - 4 + 3];
-
-            if (i / 4 % Nk == 0)
+            int i = 0;
+            while (i < 4 * Nk)
             {
-                RotDWord(temp);
-                SubDWordBytes(temp);
-                Rcon(rcon, i / (Nk * 4));
-                XorDWords(temp, rcon, temp);
-            }
-            else if (Nk > 6 && i / 4 % Nk == 4)
-            {
-                SubDWordBytes(temp);
+                w[i] = key[i];
+                i++;
             }
 
-            w[i + 0] = w[i - 4 * Nk] ^ temp[0];
-            w[i + 1] = w[i + 1 - 4 * Nk] ^ temp[1];
-            w[i + 2] = w[i + 2 - 4 * Nk] ^ temp[2];
-            w[i + 3] = w[i + 3 - 4 * Nk] ^ temp[3];
-            i += 4;
+            i = 4 * Nk;
+            while (i < 4 * Nb * (Nr + 1))
+            {
+                temp[0] = w[i - 4 + 0];
+                temp[1] = w[i - 4 + 1];
+                temp[2] = w[i - 4 + 2];
+                temp[3] = w[i - 4 + 3];
+
+                if (i / 4 % Nk == 0)
+                {
+                    RotDWord(temp);
+                    SubDWordBytes(temp);
+                    Rcon(rcon, i / (Nk * 4));
+                    XorDWords(temp, rcon, temp);
+                }
+                else if (Nk > 6 && i / 4 % Nk == 4)
+                {
+                    SubDWordBytes(temp);
+                }
+
+                w[i + 0] = w[i - 4 * Nk] ^ temp[0];
+                w[i + 1] = w[i + 1 - 4 * Nk] ^ temp[1];
+                w[i + 2] = w[i + 2 - 4 * Nk] ^ temp[2];
+                w[i + 3] = w[i + 3 - 4 * Nk] ^ temp[3];
+                i += 4;
+            }
+
+            delete []rcon;
+            delete []temp;
+
+            RoundedKeys = w;
         }
-
-        delete []rcon;
-        delete []temp;
-
-        RoundedKeys = w;
     }
 }
 
